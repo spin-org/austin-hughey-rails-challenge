47a1ad1db9594f2f8c421b76eb8a02c0ccb0e9a2
J. Austin Hughey <jah@jah.io>
2019-06-16 01:10:02 -0600

    Scooters Controller: Find scooters near me
    GET /scooters/near/LAT/LON/RADIUS
    
    Radius is in meters.

--- app/controllers/scooters_controller.rb
+++ app/controllers/scooters_controller.rb
@@ -1,6 +1,50 @@
 class ScootersController < ApplicationController
   before_action :set_scooter, only: [:show, :deactivate, :activate]
 
+  #
+  # Finds scooters within RADIUS meters of LAT and LON.
+  #   GET '/scooters/near/30.269362/-97.7408848/75'
+  #     would return up to 50 scooters within 75 meters of the point given
+  #     in the order of LONGITUDE (west <--> east), LATITUDE (up/down).
+  #
+  #   This API is only going to return a JSON array of scooters within the
+  #   defined radius, along with their location as longitude, latitude, and
+  #   their battery level. A timestamp will be included that tells you how
+  #   "fresh" that information is - in other words, when the scooter last
+  #   checked in.
+  #
+  #   It's up to the client application to plot these on a map and decide
+  #   whether or not the data should be presented to the user, or which
+  #   parts of that data should be presented to the user (e.g. you might
+  #   want to filter scooters that haven't checked in for over 3 days, etc.).
+  #
+  #   This will be limited to a maximum return set of 50 scooters. No more.
+  #   Usually the result set will be less but for now, that value will be
+  #   hard-coded.
+  #
+  #   Example of what this is going to return:
+  #
+  #           {
+  #             "scooters": [
+  #               {
+  #                 "scooter_id": "65363976-0dba-4a8b-823f-518850a47781",
+  #                 "battery_level": 88.76293,
+  #                 "location": "-97.7408848 30.269362",
+  #                 "last_checkin": "2019-06-16 03:58:41"
+  #               }
+  #             ]
+  #           }
+  #
+  #
+  #
+  def locate
+    lat    = params[:lat]
+    lon    = params[:lon]
+    radius = params[:radius]
+    now    = DateTime.now.utc.strftime('%Y-%m-%d %H:%M:%S')
+    render json: Scooter.near(lon: lon, lat: lat, radius: radius, after: now)
+  end
+
   def show
     render json: @scooter
   end
--- spec/controllers/scooters_controller_spec.rb
+++ spec/controllers/scooters_controller_spec.rb
@@ -1,6 +1,68 @@
 require 'rails_helper'
 
 RSpec.describe ScootersController, type: :controller do
+  describe '/scooters/near/:lat/:lon/:radius' do
+    before :each do
+      @deactivated_scooter = Scooter.create(active: false)
+      @far_scooter = Scooter.create
+      @far_scooter_report = Report.create(
+        scooter: @far_scooter,
+        battery_level: 85,
+        location: 'POINT(-97.7488463 30.269854)' # Star Bar, Austin, TX
+      )
+      @low_battery_scooter = Scooter.create
+      @low_battery_scooter_report = Report.create(
+        scooter: @low_battery_scooter,
+        battery_level: 29.4475,
+        location: 'POINT(-97.7412248 30.2688096)'
+      )
+      @scooters = []
+      @reports = []
+      3.times do
+        scooter = Scooter.create
+        @scooters << scooter
+        3.times do
+          report = Report.create(
+            scooter: scooter,
+            battery_level: 85,
+            location: 'POINT(-97.7412248 30.2688096)'
+          )
+        end
+      end
+    end
+
+    it 'reports scooters near me' do
+      get :locate, params: { radius: 75, lat: 30.2688096, lon: -97.7412248 }
+      expect(response).to be_successful
+      count = JSON.parse(response.body).count
+# I ran into a non-deterministic test failure here but could only reproduce it
+# once, and that was before calling pry on it. So I'm leaving this here for now
+# in hopes of catching the error. I suspect something having to do with time
+# and/or psql.
+if count != 3
+  binding.pry
+end
+      expect(count).to eq 3
+    end
+
+    it 'does not report inactive/deactivated scooters' do
+      get :locate, params: { radius: 75, lat: 30.2688096, lon: -97.7412248 }
+      expect(response).to be_successful
+      scooters = JSON.parse(response.body)
+      scooters.each do |s|
+        expect(s['id']) != @deactivated_scooter.id
+      end
+    end
+
+    it 'does not report scooters further than RADIUS' do
+      get :locate, params: { radius: 75, lat: 30.2688096, lon: -97.7412248 }
+      expect(response).to be_successful
+      scooters = JSON.parse(response.body)
+      scooters.each do |s|
+        expect(s['id']) != @far_scooter.id
+      end
+    end
+  end
   describe '/scooters' do
     before :each do
       Scooter.new.save
