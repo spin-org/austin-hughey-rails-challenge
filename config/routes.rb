f5edb4b7246c5cda616c06be82d3aabcdf89ffde
J. Austin Hughey <jah@jah.io>
2019-06-16 01:09:27 -0600

    Fix syntax error/typo

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,10 @@ Rails.application.routes.draw do
   put '/scooters/:id/activate', to: 'scooters#activate'
   put '/scooters/:id/deactivate', to: 'scooters#deactivate'
   put '/scooters/mass-activate', to: 'scooters#mass_activate'
+  get '/scooters/near/:lat/:lon/:radius', to: 'scooters#locate'
   post '/reports', to: 'reports#create'
   post '/tickets', to: 'tickets#create'
   get  '/tickets/:id', to: 'tickets#show'
   # Normally I'd put something like /tickets/scooter_id here too
+
 end
